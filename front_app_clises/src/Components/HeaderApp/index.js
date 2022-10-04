a32e0e11603467957ada31de52a38042bb9d7f3e
jhgarzonm <jhgarzonm@gmail.com>
2022-10-04 17:17:52 -0500

    Cambios en el ícono del menú del header

--- front_app_clises/src/Components/HeaderApp/index.js
+++ front_app_clises/src/Components/HeaderApp/index.js
@@ -3,10 +3,10 @@ import "./HeaderAppStyle.css"
 
 function HeaderApp() {
 return(
-<React.Fragment><button className="IconoMenu"><span class="material-symbols-outlined">
+<React.Fragment><header className="Header"><button className="IconoMenu"><span class="material-symbols-outlined">
 menu
 </span></button>
-<img src={"http://www.imcogroup.co/uploads/1/2/4/2/124297412/published/nuevo-logo-imco-pagina.png?1613011667"} alt={"Logotipo"}/>
+<img class="logo-empresa"src={"http://www.imcogroup.co/uploads/1/2/4/2/124297412/published/nuevo-logo-imco-pagina.png?1613011667"} alt={"Logotipo"}/></header>
 </React.Fragment>
 )
 
