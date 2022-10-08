6a6c8060aa0b5c42f71b5892aaad1a668f2fa4f3
jhgarzonm <jhgarzonm@gmail.com>
2022-10-08 09:47:18 -0500

    Header style changes. Elements centered and color changes in button

--- front_app_clises/src/Components/HeaderApp/index.js
+++ front_app_clises/src/Components/HeaderApp/index.js
@@ -5,8 +5,8 @@ function HeaderApp() {
 return(
 <React.Fragment><header className="Header"><button className="IconoMenu"><span class="material-symbols-outlined">
 menu
-</span></button>
-<img class="logo-empresa"src={"http://www.imcogroup.co/uploads/1/2/4/2/124297412/published/nuevo-logo-imco-pagina.png?1613011667"} alt={"Logotipo"}/></header>
+</span></button><img class="logo-empresa"src={"http://www.imcogroup.co/uploads/1/2/4/2/124297412/published/nuevo-logo-imco-pagina.png?1613011667"} alt={"Logotipo"}/>
+</header>
 </React.Fragment>
 )
 
