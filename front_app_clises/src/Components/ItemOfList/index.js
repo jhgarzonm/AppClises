130dbb65cb46195c0f315f164ae839c79e64e3ec
jhgarzonm <jhgarzonm@gmail.com>
2022-10-08 15:29:38 -0500

    Style changes in the main section, title, description and date

--- front_app_clises/src/Components/ItemOfList/index.js
+++ front_app_clises/src/Components/ItemOfList/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./style.css";
 
 function ItemOfList(props) {
   return (
     <React.Fragment>
-      <li className="">
-        {props.task}
-        <input type="checkbox" />
+      <div className="Container">
+      <li className="Título">
+        {props.task}   
+        <div className="checkbox"><input className="input1" type="checkbox" /></div>
       </li>
+      <p className="Descripción">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris </p>
+        <p className="Fecha">20/12/20</p>
+        </div>
     </React.Fragment>
   );
 }
